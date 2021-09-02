aa9a4c68c24f3d778bbb7d73bd7035cbd3271a81
jamesvidler <james.vidler@outlook.com>
2021-09-02 18:25:40 -0400

    testing build fix for vercel

--- components/agility-pageModules/DynamicArticleDetails.js
+++ components/agility-pageModules/DynamicArticleDetails.js
@@ -10,7 +10,7 @@ const DynamicArticleDetails = ({ module, dynamicPageItem }) => {
     <div className="flex flex-row justify-center">
      
       <div className="relative px-8">
-        <div className="max-w-2xl mx-auto my-12 md:mt-18 lg:mt-20">
+        <div className="text-lg max-w-prose mx-auto">
             <h1>
                 <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                     {dynamicPageItem.fields.title}
