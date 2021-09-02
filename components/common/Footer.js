0e69afd02373b0a49db09db45acae16f4ba3e36a
jamesvidler <james.vidler@outlook.com>
2021-09-02 18:37:06 -0400

    fixed unnamed component

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -73,7 +73,7 @@ const navigation = {
     ],
   }
   
-  export default function Example() {
+  const Footer = () => {
     return (
       <footer className="bg-white">
         <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
@@ -99,4 +99,6 @@ const navigation = {
       </footer>
     )
   }
+
+  export default Footer;
   
