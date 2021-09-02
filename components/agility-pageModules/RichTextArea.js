1b6beb3a3e6924ea33c7033fe0b0d1816fb2ff89
jamesvidler <james.vidler@outlook.com>
2021-09-02 11:40:12 -0400

    POC of article sidebar

--- components/agility-pageModules/RichTextArea.js
+++ components/agility-pageModules/RichTextArea.js
@@ -1,19 +1,28 @@
 import React, { useEffect } from "react";
 import { renderHTML } from "@agility/nextjs";
+import ArticleNav from '../common/ArticleNav'
 
 const RichTextArea = ({ module }) => {
   // get module fields
   const { fields } = module;
 
   return (
-    <div className="relative px-8">
-      <div className="max-w-2xl mx-auto my-12 md:mt-18 lg:mt-20">
-        <div
-          className="prose max-w-full mx-auto"
-          dangerouslySetInnerHTML={renderHTML(fields.textblob)}
-        />
+    <div className="flex flex-row justify-center">
+     
+      <div className="relative px-8">
+        <div className="max-w-2xl mx-auto my-12 md:mt-18 lg:mt-20">
+          <div
+            className="prose max-w-full mx-auto"
+            dangerouslySetInnerHTML={renderHTML(fields.textblob)}
+          />
+        </div>
+        
       </div>
+      <div className="hidden lg:block sticky top-0 w-60 flex-none max-h-96 py-12">
+          <ArticleNav />
+        </div>
     </div>
+    
   );
 };
 
--- components/agility-pageTemplates/MainTemplate.js
+++ components/agility-pageTemplates/MainTemplate.js
@@ -5,8 +5,8 @@ import Sidenav from '../common/Sidenav'
 
 const MainTemplate = (props) => {
   return (
-    <div id="MainTemplate" className="flex flex-grow bg-white lg:px-2 overflow-hidden">
-      <div className="hidden lg:flex lg:flex-shrink-0 overflow-y-auto">
+    <div id="MainTemplate" className="flex flex-grow bg-white overflow-hidden">
+      <div className="hidden lg:px-2 lg:flex lg:flex-shrink-0 overflow-y-auto">
         <Sidenav />
       </div>
       <div className="flex-grow overflow-y-auto">
