c132de4212b7efade7146f8757de430e971fe420
jamesvidler <james.vidler@outlook.com>
2021-09-07 16:24:07 -0400

    fix to fonts

--- components/common/HeadSEO.js
+++ components/common/HeadSEO.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import ReactHtmlParser from "react-html-parser";
-import Script from 'next/script'
 //import nightwind from 'nightwind/helper'
 
 const HeadSEO = ({ title, description, keywords, ogImage, metaHTML }) => {
@@ -18,17 +17,6 @@ const HeadSEO = ({ title, description, keywords, ogImage, metaHTML }) => {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="description" content={description} />
       <meta name="keywords" content={keywords} />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
-        rel="stylesheet"
-      />
-      {process.env.ROBOTS_NO_INDEX &&
-        <meta name="robots" content="noindex"></meta>
-      }
-      {/* <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} /> */}
-      {/* Start of agilitycms Zendesk Widget script */}
-      <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=75a855ec-8bb9-4017-a4d7-ebf0e3d7c77a"> </Script>
-      {/* End of agilitycms Zendesk Widget script */}
       {ogImage && <meta property="og:image" content={ogImage} />}
       {additionalHeaderMarkup}
     </Head>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -10,7 +11,18 @@ class MyDocument extends Document {
     return (
       <Html className="h-full">
         <Head>
+            {process.env.ROBOTS_NO_INDEX &&
+              <meta name="robots" content="noindex"></meta>
+            }
+            {/* <script dangerouslySetInnerHTML={{ __html: nightwind.init() }} /> */}
+            {/* Start of agilitycms Zendesk Widget script */}
+            <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=75a855ec-8bb9-4017-a4d7-ebf0e3d7c77a"> </Script>
+            {/* End of agilitycms Zendesk Widget script */}
             <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
+                rel="stylesheet"
+            />
         </Head>
         <body className="overflow-hidden h-full">
           <Main />
