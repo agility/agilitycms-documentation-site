41614c1e74854e9f16f20de21cbaf54eb9c28118
jamesvidler <james.vidler@outlook.com>
2021-09-07 17:00:42 -0400

    test without google inter font

--- pages/_document.js
+++ pages/_document.js
@@ -19,10 +19,10 @@ class MyDocument extends Document {
             <Script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=75a855ec-8bb9-4017-a4d7-ebf0e3d7c77a"> </Script>
             {/* End of agilitycms Zendesk Widget script */}
             <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-            <link
+            {/* <link
               href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;900&display=swap"
                 rel="stylesheet"
-            />
+            /> */}
         </Head>
         <body className="overflow-hidden h-full">
           <Main />
