78b7765143cc5f1bcf0df1d7fd7d31cf32bd4474
jamesvidler <james.vidler@outlook.com>
2021-09-15 15:24:36 -0400

    styled code block

--- components/common/blocks/Code.js
+++ components/common/blocks/Code.js
@@ -2,9 +2,11 @@ import React from "react";
 
 const Code =  ({ id, code}) => {
     return (
-        <code className="block whitespace-pre overflow-x-auto mb-8 mt-8">
-            {code}
-        </code>
+        <pre>
+            <code className="block rounded-lg bg-gray-100 text-gray-700 px-5 py-5 whitespace-pre overflow-x-auto mb-8 mt-8">
+                {code}
+            </code>
+        </pre>
     );
 };
 
