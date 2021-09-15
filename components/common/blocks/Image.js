43fb12b26abfee957ecd02e4536f7013abe58f6e
jamesvidler <james.vidler@outlook.com>
2021-09-15 16:48:08 -0400

    rounded image corners + border

--- components/common/blocks/Image.js
+++ components/common/blocks/Image.js
@@ -6,7 +6,7 @@ const Image =  ({ id, caption, file, stretched, withBackground, withBorder }) =>
     const size = file.size;
     return (
         <div>
-            <img className="m-auto" src={file.url} alt={caption} />
+            <img className="m-auto rounded-lg border" src={file.url} alt={caption} />
         </div>
     );
 };
