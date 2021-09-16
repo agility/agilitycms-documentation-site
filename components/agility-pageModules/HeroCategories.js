fd3a992b0c6faba1d9aff205988122cbbb11a916
jamesvidler <james.vidler@outlook.com>
2021-09-16 12:44:27 -0400

    added contrast on home hero

--- components/agility-pageModules/HeroCategories.js
+++ components/agility-pageModules/HeroCategories.js
@@ -25,18 +25,17 @@ export default function HeroCategories({ module }) {
     return (
         <div className="bg-white">
         {/* Header */}
-        <div className="relative pb-32 bg-gray-100">
+        <div className="relative pb-32 bg-gray-900">
             <div className="absolute inset-0">
-            {/* <img
-                className="w-full h-full object-cover"
-                src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&&sat=-100"
-                alt=""
-            /> */}
-            
+                {/* <img
+                    className=""
+                    src="/docs/assets/bg.png"
+                    alt=""
+                /> */}
             </div>
             <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-            <h1 className="text-center text-4xl font-extrabold tracking-tight text-gray-900 md:text-5xl lg:text-6xl">{fields.title}</h1>
-            <p className="text-center mt-6 text-xl text-gray-700">
+            <h1 className="text-center text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">{fields.title}</h1>
+            <p className="text-center mt-6 text-xl text-white">
                 {fields.subTitle}
             </p>
             </div>
