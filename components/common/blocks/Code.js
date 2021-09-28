382a4634d7c3037c68134517f5040bc32b9e8661
jamesvidler <james.vidler@outlook.com>
2021-09-28 13:43:21 -0400

    reduced font-size on code blocks

--- components/common/blocks/Code.js
+++ components/common/blocks/Code.js
@@ -2,7 +2,7 @@ import Highlight from 'react-highlight'
 
 const Code =  ({ id, code}) => {
     return (
-        <Highlight className="block rounded-lg bg-gray-100 p-5 my-8">
+        <Highlight className="block text-sm rounded-lg bg-gray-100 p-5 my-8">
             {code}
         </Highlight>
     );
