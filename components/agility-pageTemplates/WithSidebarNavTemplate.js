aa41b1adfc4b3feae88cd289f95143bbf4ad6030
jamesvidler <james.vidler@outlook.com>
2021-10-28 16:01:29 -0400

    added miissing prop

--- components/agility-pageTemplates/WithSidebarNavTemplate.js
+++ components/agility-pageTemplates/WithSidebarNavTemplate.js
@@ -24,7 +24,7 @@ const WithSidebarNavTemplate = (props) => {
             getModule={getModule}
           />
         </div>
-        <Footer navigation={props.footerNavigation} bottomNavigation={props.footerBottomNavigation} />
+        <Footer navigation={props.footerNavigation} bottomNavigation={props.footerBottomNavigation} pageTemplateName={props.pageTemplateName} />
       </div>
     </div>
   );
