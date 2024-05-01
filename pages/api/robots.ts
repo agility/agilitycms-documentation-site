0581d1d637e6931c9d1ba147276abf1bfd48d509
Joel Varty <joel@agilitycms.com>
2024-05-01 17:45:43 -0400

    vary

--- pages/api/robots.ts
+++ pages/api/robots.ts
@@ -9,12 +9,14 @@ export default function handler(req: NextApiRequest,
 	if (cdnLoop === "netlify") {
 		//only allow crawling on agility domains
 		res.setHeader("Content-Type", "text/plain")
+			.setHeader("Vary", "cdn-loop")
 			.setHeader("Cache-Control", "public, max-age=86400")
 			.send("User-agent: *\nAllow: /")
 	} else {
 
 		//disallow any crawling on non-agility domains
 		res
+			.setHeader("Vary", "cdn-loop")
 			.setHeader("X-CDN-Loop", cdnLoop)
 			.setHeader("X-Host", host)
 			.setHeader("Content-Type", "text/plain")
